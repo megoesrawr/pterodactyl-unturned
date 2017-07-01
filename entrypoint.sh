633a0e23ff468f86d858536e9999378715c2fd0e
Unknown <isaacalich8401@gmail.com>
2017-07-01 14:40:38 -0400

    Dockerfile & entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+# Update Unturned Server
+./steam/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 304930 +quit
+echo "Downloading RocketMod..."
+curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
