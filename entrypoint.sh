93e70f0dfaf869e806a9506cd2e3c4213a85baf2
Unknown <isaacalich8401@gmail.com>
2017-07-02 09:35:13 -0400

    Screw that

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,14 @@ echo "Downloading RocketMod..."
 curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
 unzip -o -q Rocket.zip
 
-if [ -z "${ALLOC_0__PORT}" ] || [ "$((${ALLOC_0__PORT}-1))" != "${SERVER_PORT}" ]; then
-    echo "
-======================================
-
-You need to add port $((${SERVER_PORT}+1)) as an
- additional allocation to the server.
-
-======================================
-"
+if [ -z "${ALLOC_0__PORT}" ] || [ "$((ALLOC_0__PORT-1))" != "${SERVER_PORT}" ]; then
+    echo "---=== Please add port $((SERVER_PORT+1)) to the server as an additional allocation, or you will be unable to connect. ===---"
     sleep 10
     exit 1
 fi
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
