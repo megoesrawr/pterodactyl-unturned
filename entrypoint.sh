f600c7a487d3df70ff2584f50b07bb73dfd956ea
Unknown <isaacalich8401@gmail.com>
2017-07-01 21:13:51 -0400

    Allow for steam login & delete README

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ sleep 2
 cd /home/container
 
 # Update Unturned Server
-./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 32 +login anonymous +force_install_dir /home/container +app_update 304930 +quit
+./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 32 +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update 304930 +quit
 echo "Downloading RocketMod..."
 curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
-unzip Rocket.zip
+unzip -o -q Rocket.zip
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
