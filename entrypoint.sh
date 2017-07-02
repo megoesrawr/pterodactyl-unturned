0d887a05ed3e66d7be9991123489def2ac51da81
Unknown <isaacalich8401@gmail.com>
2017-07-01 20:47:19 -0400

    Force 32-bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ sleep 2
 cd /home/container
 
 # Update Unturned Server
-./steam/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 304930 +quit
+./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 32 +login anonymous +force_install_dir /home/container +app_update 304930 +quit
 echo "Downloading RocketMod..."
 curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
 unzip Rocket.zip
