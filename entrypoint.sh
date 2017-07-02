56960d253be894df7e34a4145fae510fac73704f
Unknown <isaacalich8401@gmail.com>
2017-07-02 09:14:57 -0400

    A different attempt

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,14 @@ curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuc
 unzip -o -q Rocket.zip
 
 if [ -z "${ALLOC_0__PORT}" ] || [ "$((${ALLOC_0__PORT}-1))" != "${SERVER_PORT}" ]; then
-    printf "\n\n======================================"
-    printf "\n\n"
-    printf "You need to add port $((${SERVER_PORT}+1)) as an\n"
-    printf " additional allocation to the server."
-    printf "\n\n"
-    printf "======================================\n\n"
+    echo "
+======================================
+
+You need to add port $((${SERVER_PORT}+1)) as an
+ additional allocation to the server.
+
+======================================
+"
     sleep 10
     exit 1
 fi
