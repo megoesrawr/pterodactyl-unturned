2ddf05646e0cb157871b57c302e09a5b24c6588c
Unknown <isaacalich8401@gmail.com>
2017-07-01 20:39:44 -0400

    Actually unzip RocketMod

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ cd /home/container
 ./steam/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 304930 +quit
 echo "Downloading RocketMod..."
 curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
+unzip Rocket.zip
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
