e912b52e45d730af3de9d2a90d8c6b7e4e5faea7
Isaac A <isaacalich8401@gmail.com>
2018-03-17 09:25:10 -0400

    Workaround Pterodactyl Bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,11 @@ echo "Downloading RocketMod..."
 curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned%20Linux/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
 unzip -o -q Rocket.zip
 
-if [ -z "${ALLOC_0__PORT}" ] || [ "$((ALLOC_0__PORT-1))" != "${SERVER_PORT}" ]; then
-    printenv                    
-    echo "---=== Please add port $((SERVER_PORT+1)) to the server as an additional allocation, or you will be unable to connect. ===---"
-    sleep 10
-    exit 1
-fi
+#if [ -z "${ALLOC_0__PORT}" ] || [ "$((ALLOC_0__PORT-1))" != "${SERVER_PORT}" ]; then
+#    echo "Please add port $((SERVER_PORT+1)) to the server as an additional allocation, or you will be unable to connect."
+#    sleep 10
+#    exit 1
+#fi
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
